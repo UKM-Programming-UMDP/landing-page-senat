5e036e21cc8a56d2ad2491a4d13e145c52be2643
XITKJxav <jeflijonataha1@gmail.com>
2025-06-20 19:41:02 +0700

    Remove unused code form about page

--- src/views/about/index.jsx
+++ src/views/about/index.jsx
@@ -5,17 +5,9 @@ import { BadanKepengurusan } from "./components/badanKepengurusan";
 import { Kabinet } from "./components/kabinet";
 import { Profile } from "./components/profile";
 import { Koordinator } from "./components/koordinator";
-import { useEffect } from "react";
 import { FullpageSection } from "@ap.cx/react-fullpage";
 
 export const About = () => {
-  // useEffect(() => {
-  //   window.scrollTo({
-  //     top: 0,
-  //     behavior: "smooth",
-  //   });
-  // }, []);
-
   const listItems = [<Profile />, <Kabinet />, <VisiMisi />, <ProgramKerja />];
   return (
     <HelmetProvider>
@@ -31,21 +23,4 @@ export const About = () => {
       <Koordinator />
     </HelmetProvider>
   );
-
-  // return (
-  //   <HelmetProvider>
-  //     <Helmet>
-  //       <title>About Page</title>
-  //     </Helmet>
-
-  //     <Profile />
-  //     <Kabinet />
-  //     <div className="overflow-hidden grid gap-y-40">
-  //       <VisiMisi />
-  //       <BadanKepengurusan />
-  //       <Koordinator />
-  //       <ProgramKerja />
-  //     </div>
-  //   </HelmetProvider>
-  // );
 };
